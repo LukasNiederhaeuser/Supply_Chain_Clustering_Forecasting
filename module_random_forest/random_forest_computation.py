b43900b15224d3dfd8e26d486123e6aa26390f59
Lukas Niederhäuser <lukas.niederhauser@hotmail.com>
2023-10-14 18:38:35 +0200

    Adjust rf module

--- module_random_forest/random_forest_computation.py
+++ module_random_forest/random_forest_computation.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import sys
 import os
-# Add the path to the module to the system path
+# Import the costum module
 module_path = os.path.join(os.getcwd(), "module_data_prep_forecast")
 sys.path.append(module_path)
 import data_preparation_forecasting
@@ -12,9 +12,8 @@ def get_data():
 
     # Call the main function to obtain the dataframes
     df_list = data_preparation_forecasting.main()
+    
+    return df_list
 
-
-    # Store the dataframes accordingly
-    df_fanshop = df_list[0]
-    df_apparel = df_list[1]
-    df_golf = df_list[2]
+df_list = get_data()
+print(df_list[0].head(5))
